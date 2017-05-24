6e631310096a27f21078a8730c5e8678b3b3f327
dxsfung <davxsfung@gmail.com>
2017-05-24 14:31:59 +0800

    render html instead of render text

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render text: "hello, thank you!"
+    render html: "hello, thank you!"
   end
 
 end
